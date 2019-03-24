4d24deb6ff8c2a3504715434e3db7ee4ed047b94
Joshua Vance <skinsfandc0612@icloud.com>
2019-03-24 16:59:08 -0400

    homepage is looking good now

--- src/component/App/App.js
+++ src/component/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from '../Home/Home';
+import '../Home/Home.css';
 
 class App extends Component {
   render() {
     return (
-      <div className='App'>
+      <div className='Home'>
         <Home />
       </div>
     );
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,31 +1,89 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Form from 'muicss/lib/react/form';
+import Input from 'muicss/lib/react/input';
+import Textarea from 'muicss/lib/react/textarea';
+import Button from 'muicss/lib/react/button';
 
-class App extends Component {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    alert(this.state.username);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <Router>
         <div className='App'>
           <header className='App-header'>
-            <Link to='/'>Home</Link>
+            <div>
+              <img src='' className='app-logo' alt='logo' />
+              <ul>
+                <Link className='header-link' to='/login'>
+                  Drop in
+                </Link>
 
-            <Link to='/about'>About</Link>
+                <Link className='header-link' to='/signup'>
+                  Sign up
+                </Link>
 
-            <Link to='/topics'>Topics</Link>
+                <Link className='header-link' to='/help'>
+                  Help
+                </Link>
 
-            <hr />
+                <Link className='header-link' to='/looHumor'>
+                  Loo Humor
+                </Link>
 
-            {/* <Route exact path='/' component={Home} />
-            <Route path='/about' component={About} /> */}
-            {/* <Route path='/topics' component={Topics} /> */}
-            <img src='' className='App-logo1' alt='logo' />
-            <img src='' className='App-logo2' alt='logo' />
+                <Link className='header-link' to='/looCation'>
+                  Loo-Cation
+                </Link>
+
+                <Link className='header-link' to='/looCator'>
+                  Loo-Cator
+                </Link>
+              </ul>
+            </div>
+            {/* <Route exact path='/login' component={login} />
+            <Route path='/signup' component={signup} />
+            <Route path='/help' component={help} />
+            <Route path='/looHumor' component={looHumor} />
+            <Route path='/looCation' component={looCation} />
+            <Route path='/looCator' component={looCator} /> */}
           </header>
-          <body className='body'>Something is here</body>
         </div>
+        <div className='card-body'>
+          <div className='card-body' style={{ textAlign: 'center' }} />
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type='text'
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <input type='submit' value='Submit' />
+          </form>
+        </div>
+        <Form>
+          <legend>Title</legend>
+          <Input placeholder='Input 1' />
+          <Input placeholder='Input 2' />
+          <Textarea placeholder='Textarea' />
+          <Button variant='raised'>Submit</Button>
+        </Form>
       </Router>
     );
   }
 }
 
-export default App;
+export default Home;
